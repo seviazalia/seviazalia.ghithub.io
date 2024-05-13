9da5c01cea7315d956202dbac0cb10b7f4f0138c
seviazalia <168519762+seviazalia@users.noreply.github.com>
2024-05-13 13:46:22 +0700

    Create tugaskondisi.js

---
+++ tugaskondisi.js
@@ -0,0 +1,159 @@
+const usia =100;
+if (usia >80) {
+    console.log("usia anda lebih besar dari 80");
+} else {
+     console.log("usia anda lebih kecil dari 80");
+}  
+
+const berat =50;
+if (berat >45) {
+    console.log("berat anda lebih dari 45");
+} else {
+    console.log("berat anda lebih kecil dari 45");
+}
+
+const tinggi =60;
+if (tinggi >175) {
+    console.log("tinggi anda lebih besar dari 175");
+} else {
+     console.log("tinggi anda lebih kecil dari 175");
+}
+
+const jam =15.15;
+if (jam >14.17) {
+    console.log("jam lebih besar dari 14.17");
+} else {
+     console.log("jam lebih kecil dari 15.15");
+}
+
+const angka =8;
+if (angka <6) {
+     console.log("angka lebih dari 6");
+} else {
+     console.log("angka tidak lebih dari 6");
+     }
+     
+    
+    
+    
+
+var i= 8;
+var withoutternary;
+
+    
+
+if (i>5){
+   withoutternary = "greater than 10";
+} else {
+  withoutternary = "less than 10";
+}
+
+const ternaryanswer  = i > 5 ? "greater than 5" : "les than 5";
+const ternarywithif  = i > 5 ? "greater than 10" :i < 8 ? "less than 8" : "between 8 and 5";
+console.log(withoutternary)
+console.log(ternaryanswer)
+console.log(ternarywithif)
+
+
+var i= 15;
+var withoutterrnary;
+
+    
+
+if (i>7){
+   withoutterrnary = "greater than 10";
+} else {
+  withoutterrnary = "less than 10";
+}
+
+const ternarryanswer  = i > 10 ? "greater than 7" : "les   than 7";
+const ternarrywithif  = i > 10 ? "greater than 10" :i < 15 ? "less than 7" : "between 7 and 15";
+console.log(withoutterrnary)
+console.log(ternarryanswer)
+console.log(ternarrywithif)
+
+
+var i= 4;
+var withoutterrnary;
+
+    
+
+if (i>6){
+   withoutterrnary = "greater than 10";
+} else {
+  withoutterrnary = "less than 10";
+}
+
+const ternarryanswerr  = i > 6 ? "greater than 6" : "les   than 6";
+const ternarrywithiff = i > 6 ? "greater than 10" :i < 4 ? "less than 6" : "between 6 and 4";
+console.log(withoutterrnary)
+console.log(ternarryanswer)
+console.log(ternarrywithif)
+
+
+var i= 2;
+var withoutterrnary;
+
+    
+
+if (i>1){
+   withoutterrnary = "greater than 10";
+} else {
+  withoutterrnary = "less than 10";
+}
+
+const ternaryanswerr  = i > 1 ? "greater than 1" : "les   than 1";
+const ternarrywithiiff = i > 1 ? "greater than 10" :i < 2 ? "less than 1" : "between 1 and 2";
+console.log(withoutterrnary)
+console.log(ternarryanswer)
+console.log(ternarrywithif)
+
+
+var i= 4;
+var withoutterrnary;
+
+    
+
+if (i>11){
+   withoutterrnary = "greater than 10";
+} else {
+  withoutterrnary = "less than 10";
+}
+
+const ternaryansweerr  = i > 11 ? "greater than 1" : "les   than 11";
+const tternarrywithiif = i > 11 ? "greater than 10" :i < 4 ? "less than 1" : "between 11 and 4";
+console.log(withoutterrnary)
+console.log(ternarryanswer)
+console.log(ternarrywithif)
+
+
+
+
+///ternarywithif///
+
+//nilai//
+var i= 46;//variabel yang ingin dimasukkan//
+const ternarywithiff = i > 80 ? "Lulus" : i < 65? "Remidi": "Tidak Lulus";//?=kemungkinan pertama(if),:=if else, : terakhir=else//
+
+//suhu//
+var x= 21;
+const celsius = x > 25 ? "Panas" : x > 20? "Normal": "Dingin";
+
+//usia//
+var y= 20;
+const lama = i > 60 ? "Lansia" : i > 20? "Dewasa": "Anak-anak";
+
+//tinggi badan//
+var z= 155;
+const pendek = z > 165 ? "Tinggi" : z > 150? "Normal": "Pendek";
+
+//berat badan//
+var r= 50;
+const ringan = r > 80 ? "Obesitas"  : r > 45? "Normal": "Kurang Gizi";
+
+//kode untuk menampilkan hasil//
+console.log(ternarywithif)
+console.log(celsius)
+console.log(lama)
+console.log(pendek)
+console.log(ringan)
